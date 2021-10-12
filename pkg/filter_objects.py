3bfbdebd9866c1f4d4a61e108ba712d7ef6d4deb
jtsorren <jtsorren@eng.ucsd.edu>
2021-10-12 13:25:50 -0700

    anova update

--- pkg/filter_objects.py
+++ pkg/filter_objects.py
@@ -6,7 +6,7 @@ import itertools
 import copy
 
 def ANOVA_oneway(values, labels, measures, conditions, alpha, method):
-    split_data = list([np.stack([values[i] for i in range(len(labels)) if condition in labels[i]], axis=0) for condition in conditions])
+    split_data = list([np.stack([values[i] for i in range(len(labels)) if condition in labels[i] and condition[0]==labels[i][0]], axis=0) for condition in conditions])
     test = f_oneway(*split_data)
     hyp_vec = multipletests(pvals=test.pvalue, alpha=alpha, method=method)[0]
     return hyp_vec
